4e440a42a451629034a836224d837cc801199b40
RodrigoRoldan-09 <rodrigoem.roldan@gmail.com>
2025-05-26 03:54:50 -0600

    Fix unused React import in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AdminInterface from './components/AdminInterface';
 import UserInterface from './components/UserInterface';
